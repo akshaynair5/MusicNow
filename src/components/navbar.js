e0ffdc7c5776e5862ea18c6101c8be38478d50b4
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-01-14 21:28:24 +0400

    1.1

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,37 +4,52 @@ import axios from 'axios'
 import { useState } from 'react'
 
 function Navbar(){
-    const [searchbarvis,setsearchvis] = useState("hidden")
-    const Search = () =>{
-        setsearchvis("visible")
+    const [searchbarvis,setSearchvis] = useState("hidden")
+    const [searchfor,setSearchfor] = useState("")
+    const [searchRes,setSearchRes] = useState("")
+    const [searchImg,setSearchImg] = useState("")
+    const SearchPop = () =>{
+        if(searchbarvis == "hidden"){
+            setSearchvis("visible")
+        }
+        else if(searchbarvis == "visible"){
+            setSearchvis("hidden")
+        }
     }
-    // const [songs,setSongs] = useState([]);
-    //     const options = {
-    //         method: 'GET',
-    //         url: 'https://shazam.p.rapidapi.com/songs/list-recommendations',
-    //         params: {key: '484129036', locale: 'en-US'},
-    //         headers: {
-    //           'X-RapidAPI-Key': 'c83210c3fdmsh36782caf2862e4cp179581jsn5c571e58597e',
-    //           'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-    //         }
-    //       };
-          
-    //       axios.request(options).then(function (response) {
-    //         //   console.log(response.data.tracks)
-
-    //           setSongs((response.data.tracks).json)
-    //           console.log(songs)
-
-    //       }).catch(function (error) {
-    //           console.error(error);
-    //       });
+    const HandleEnterKeyPress = async (e) => {
+        if(e.keyCode === 13){
+              const options = {
+                method: 'GET',
+                url: 'https://spotify23.p.rapidapi.com/search/',
+                params: {
+                  q: `${searchfor}`,
+                  type: 'multi',
+                  offset: '0',
+                  limit: '10',
+                  numberOfTopResults: '5'
+                },
+                headers: {
+                  'X-RapidAPI-Key': 'cd28ad8b44mshcc1effd9b5e2cdfp126aedjsn8167bffaf849',
+                  'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+                }
+              };
+              
+              axios.request(options).then(function (response) {
+                  setSearchImg(response.data.topResults.items[0].data.visuals.avatarImage.sources[0].url)
+                  setSearchRes(response.data.topResults.items[0].data.profile.name)
+                  console.log(response.data.topResults.items[0].data);
+              }).catch(function (error) {
+                  console.error(error);
+              });
+        }
+    }
     return(
         <div className='hero'>
         {/* code for sidebar */}
             <div className='navbar'>
                 <div className='inputs'>
                     <input value="Home" type="button"></input>
-                    <input value="Search" type="button" onClick={()=>Search()}></input>
+                    <input value="Search" type="button" onClick={()=>SearchPop()}></input>
                     <input value="Favorites" type="button"></input>
                     <input value="PlayLists" type="button" ></input>
                 </div>
@@ -49,8 +64,14 @@ function Navbar(){
                 }
             </div> */}
             <div className='content'>
-                <div className='searchbox' >
-                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" style={{position:"absolute",top:"40%",width:"60%",left:"20%",height:"20%",fontSize:"20px",visibility:searchbarvis}}/>
+                <div className='searchbox' onChange={(e)=>{setSearchfor(e.target.value)}} onKeyUp={HandleEnterKeyPress}>
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" style={{position:"absolute",top:"7%",width:"60%",left:"20%",height:"20%",fontSize:"20px",visibility:searchbarvis,TransitionEvent:'0.5s'}}/>
+                    <div className='searchResultTab' style={{visibility:searchbarvis}}>
+                        <div className='songCard' style={{backgroundColor:'white',padding:'1%'}}>
+                            <img src={`${searchImg}`} style={{ width:'90%',height:'80%'}}></img>
+                            <p>{searchRes}</p>
+                        </div>
+                    </div>
                 </div>
                 {/* <h2 className='headings'>New Releases</h2> */}
                 <div className='latestsongs'>
